9d34e0f8bbad1f77527e5a6d06fea26177fda247
luketl98 <luketl98@hotmail.com>
2023-03-30 18:17:06 +0100

    mile2_task3_step2done

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,13 @@
-class DatabaseConnector:
+import yaml
+from yaml.loader import SafeLoader
     
+class DatabaseConnector:
+    def read_db_creds():
+        with open('db_creds.yaml', 'r') as file:
+            data = yaml.load(file, Loader=yaml.SafeLoader)
+            print(data)
+            print(type(data))
+
+
+dbcon = DatabaseConnector
+dbcon.read_db_creds()

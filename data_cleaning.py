4ffae7189cf80d5f30366afb88474b5c3fc43b2b
luketl98 <luketl98@hotmail.com>
2023-06-06 23:21:55 +0100

    data_cleaning & data_extraction refactored

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,226 +3,113 @@ import pandas as pd
 from dateutil.parser import parse, ParserError
 import re
 import phonenumbers
-from pandas.api.types import is_numeric_dtype
 
 class DataCleaning:
     @staticmethod
     def clean_user_data(df):
-        df = DataCleaning.clean_alphabetical_columns(df)
-        df = DataCleaning.clean_phone_numbers(df)
-        df = DataCleaning.clean_dates(df)
-        return df
+        return df.pipe(DataCleaning.clean_alphabetical_columns).pipe(DataCleaning.clean_phone_numbers).pipe(DataCleaning.clean_dates)
 
     @staticmethod
     def clean_alphabetical_columns(df):
-        columns_to_clean = ['first_name', 'last_name']
-        for col in columns_to_clean:
-            df[col] = df[col].str.replace('[^a-zA-Z]', '')
+        df[['first_name', 'last_name']] = df[['first_name', 'last_name']].applymap(lambda x: re.sub('[^a-zA-Z]', '', x))
         return df
-    
+
     def clean_card_data(self, df):
-        df = df.copy()  # Add this line to make a copy of the DataFrame
+        known_providers = ['Diners Club / Carte Blanche', 'American Express', 'JCB 16 digit', 'JCB 15 digit', 'Maestro', 'Mastercard', 'Discover', 'VISA 19 digit', 'VISA 16 digit', 'VISA 13 digit']
 
-        # Define the list of known card providers
-        known_providers = ['Diners Club / Carte Blanche', 'American Express', 'JCB 16 digit', 'JCB 15 digit', 
-                           'Maestro', 'Mastercard', 'Discover', 'VISA 19 digit', 'VISA 16 digit', 'VISA 13 digit']
-
-        # Remove card_number with invalid length
-        df = df[df['card_number'].apply(lambda x: 13 <= len(str(x).replace('.0', '')) <= 19)]
-
-        # Convert expiry_date to datetime and remove invalid dates
-        df['expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%y', errors='coerce')
-        df = df[df['expiry_date'].notna()]
-        df = df[df['expiry_date'] > pd.to_datetime('today')]
-
-        # Keep only known card providers
-        df = df[df['card_provider'].isin(known_providers)]
-
-        # Convert date_payment_confirmed to datetime and remove invalid dates
+        df = df[df['card_number'].str.len().between(13, 19, inclusive='both')]
+        df.loc[:, 'expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%y', errors='coerce')
+        df = df.loc[df['expiry_date'].notna() & (df['expiry_date'] > pd.to_datetime('today')) & df['card_provider'].isin(known_providers)]
         df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce')
-        df = df[df['date_payment_confirmed'].notna()]
-        df = df[df['date_payment_confirmed'] < pd.to_datetime('today')]
+        df = df.loc[df['date_payment_confirmed'].notna() & (df['date_payment_confirmed'] < pd.to_datetime('today'))]
 
         return df
-    
+
     @staticmethod
     def clean_phone_numbers(df):
         def clean_number(row):
-            country_code = row['country_code']
-            raw_number = row['phone_number']
-
-            # Skip rows with NaN or empty values
-            if pd.isna(raw_number) or raw_number == '':
+            if pd.isna(row['phone_number']) or row['phone_number'] == '':
                 return pd.NA
 
-            # Remove extensions (e.g., "x1234")
-            raw_number = re.sub(r'x\d+', '', raw_number)
-
-            # Remove "(0)" often used in domestic dialing within the UK and Germany
-            raw_number = re.sub(r'\(0\)', '', raw_number)
-
-            # Remove all non-numeric characters
-            raw_number = re.sub('[^0-9]', '', raw_number)
-
-            # If the number starts with the country code, remove it
-            if country_code == 'DE' and raw_number.startswith('49'):
-                raw_number = raw_number[2:]
-            elif country_code == 'GB' and raw_number.startswith('44'):
-                raw_number = raw_number[2:]
-            elif country_code == 'US' and raw_number.startswith('1'):
-                raw_number = raw_number[1:]
-
-            # If the number now starts with a 0, remove it
+            raw_number = re.sub(r'x\d+|\(0\)|[^0-9]', '', row['phone_number'])
             raw_number = raw_number.lstrip('0')
 
-            # Add the country code back to the number
-            if country_code == 'DE':
-                raw_number = '49' + raw_number
-            elif country_code == 'GB':
-                raw_number = '44' + raw_number
-            elif country_code == 'US':
-                raw_number = '1' + raw_number
+            country_code_dict = {'DE': '49', 'GB': '44', 'US': '1'}
+            if raw_number.startswith(country_code_dict.get(row['country_code'], '')):
+                raw_number = raw_number[len(country_code_dict.get(row['country_code'], '')):]
 
-            # Parse the raw phone number using phonenumbers library
+            raw_number = country_code_dict.get(row['country_code'], '') + raw_number
+
             try:
-                parsed_number = phonenumbers.parse(raw_number, country_code)
+                parsed_number = phonenumbers.parse(raw_number, row['country_code'])
+                return phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL) if phonenumbers.is_valid_number(parsed_number) else pd.NA
             except phonenumbers.phonenumberutil.NumberParseException:
                 return pd.NA
-
-            # Check if the number is valid
-            if not phonenumbers.is_valid_number(parsed_number):
-                return pd.NA
-
-            # Format the number in international format
-            formatted_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
-
-            return formatted_number
 
         df['phone_number'] = df.apply(clean_number, axis=1)
         return df
 
-
     @staticmethod
     def clean_dates(df):
-        date_columns = ['date_of_birth', 'join_date']
-
-        @staticmethod
-        def try_parsing_date(date_str):
+        def parse_date(date_str):
             try:
-                return parse(date_str).strftime('%Y-%m-%d')
+                return parse(date_str).strftime('%Y-%m-%d') if isinstance(date_str, str) else np.nan
             except ParserError:
                 return np.nan
 
-
-        def is_valid_date(date):
-            if pd.isnull(date):
-                return False
-            if isinstance(date, str):
-                try:
-                    parse(date)
-                    return True
-                except ParserError:
-                    return False
-            return True
-
-        for col in date_columns:
-            df.loc[:, col] = df[col].apply(lambda x: try_parsing_date(x) if isinstance(x, str) else x)
-            df = df[df[col].apply(is_valid_date)]
-        
+        df[['date_of_birth', 'join_date']] = df[['date_of_birth', 'join_date']].applymap(parse_date)
         return df
-
 
     @staticmethod
     def clean_store_data(df):
-        # 1. Removing `lat` column and moving `latitude` next to `longitude`
-        df = df.drop(columns=['lat'])
-        cols = df.columns.tolist()
-        cols.insert(cols.index('longitude') + 1, cols.pop(cols.index('latitude')))
-        df = df[cols]
-        
-        # 2. Correcting the encoding issue with the `address` column
+        df.drop(columns=['lat'], inplace=True)
         df['address'] = df['address'].str.encode('latin1').str.decode('utf-8', 'ignore')
-        
-        # 3. Formatting the `opening_date` column
         df['opening_date'] = pd.to_datetime(df['opening_date'], errors='coerce')
-        
-        # 4. Removing rows with totally invalid or all null data
-        df = df.dropna(how='all')  # drop rows with all null values
-        df = df[~df.applymap(lambda x: len(str(x)) < 2 and not str(x).isnumeric()).all(axis=1)]  # drop rows with invalid data
-        
-        # 5. Correcting 'eeEurope' to 'Europe' in the `continent` column
-        df['continent'] = df['continent'].replace('eeEurope', 'Europe')
-        
-        # 6. Cleaning the `staff_numbers` column
-        df['staff_numbers'] = df['staff_numbers'].replace({'J': '1', 'e': '3'}, regex=True)
+        df.dropna(how='all', inplace=True)
+        df = df[~df.applymap(lambda x: len(str(x)) < 2 and not str(x).isnumeric()).all(axis=1)]
+        df.replace({'continent': {'eeEurope': 'Europe'}, 'staff_numbers': {'J': '1', 'e': '3'}}, inplace=True)
         df['staff_numbers'] = pd.to_numeric(df['staff_numbers'], errors='coerce').astype(pd.Int64Dtype())
-        
-        numeric_cols = ['longitude', 'latitude']  # add any other numeric columns that need cleaning
-        nan_threshold = 3
-        df = DataCleaning.clean_invalid_data(df, numeric_cols, nan_threshold)
-        
-        return df
-    
+
+        return DataCleaning.clean_invalid_data(df, ['longitude', 'latitude'], 3)
+
     @staticmethod
     def clean_invalid_data(df, numeric_cols, nan_threshold):
-        for col in numeric_cols:
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-
-        df = df[df.isnull().sum(axis=1) <= nan_threshold]
-        return df
-
+        df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
+        return df[df.isnull().sum(axis=1) <= nan_threshold]
 
     @staticmethod
     def clean_products_data(df):
-        def convert_product_weights(df):
-            def to_kg(weight):
-                if isinstance(weight, float):
-                    return weight  # It's already a float, no conversion needed
-                elif 'kg' in weight:
-                    numeric_weight = float(re.sub('[^0-9.]', '', weight))
-                elif 'g' in weight:
-                    numeric_weight = float(re.sub('[^0-9.]', '', weight)) / 1000  # Convert grams to kg
-                elif 'x' in weight:
-                    quantity, unit_weight = weight.split(' x ')
-                    if 'g' in unit_weight:
-                        numeric_weight = float(quantity) * (float(re.sub('[^0-9.]', '', unit_weight)) / 1000)  # Convert grams to kg
-                    else:
-                        numeric_weight = float(quantity) * float(re.sub('[^0-9.]', '', unit_weight))  # If kg, no need to convert
-                else:
-                    numeric_weight = None  # Handle edge cases that might have been missed
-
-                return numeric_weight
-
-            df['weight'] = df['weight'].apply(to_kg)
-            return df
-        df = convert_product_weights(df)
-
-        # Format 'date_added' column
-        def parse_dates(date):
-            try:
-                return parse(date).date()
-            except:
-                return None
-
-        df['date_added'] = df['date_added'].apply(parse_dates)
-
-        # Remove the first column
-        df = df.drop(df.columns[[0]], axis=1)
-
-        # Set any 'category' cell to null if it contains numbers
+        df['weight'] = df['weight'].apply(DataCleaning.convert_product_weights)
+        df['date_added'] = df['date_added'].apply(DataCleaning.parse_date)
+        df.drop(df.columns[0], axis=1, inplace=True)
         df.loc[df['category'].str.contains('\d', na=False), 'category'] = None
-
-        # Corrects spelling mistake in 'removed' column ('still_avaliable' to 'still_available')
-        df['removed'] = df['removed'].str.replace('Still_avaliable', 'Still_available', regex=False)
-
-        # Ensure 'removed' column contains only 'Removed' or 'Still_available', if not, set it to null
+        df.replace({'removed': {'Still_avaliable': 'Still_available'}}, inplace=True)
         df.loc[~df['removed'].isin(['Removed', 'Still_available']), 'removed'] = None
-
-        # Make sure 'product_price' contains only valid format like '£10.99', if not, set it to null
         df.loc[df['product_price'].str.contains('[a-zA-Z]', na=False), 'product_price'] = None
-
         # Remove rows where more than half of the cells are null
         half_len = len(df.columns) / 2
         df = df.dropna(thresh=half_len)
-        
+
         return df
+
+    @staticmethod
+    def convert_product_weights(weight):
+        if isinstance(weight, str):
+            if 'kg' in weight:
+                return float(re.sub('[^0-9.]', '', weight))
+            elif 'g' in weight:
+                return float(re.sub('[^0-9.]', '', weight)) / 1000
+            elif 'ml' in weight:
+                return float(re.sub('[^0-9.]', '', weight))
+            elif 'x' in weight:
+                quantity, unit_weight = weight.split(' x ')
+                return float(quantity) * (float(re.sub('[^0-9.]', '', unit_weight)) / 1000 if 'g' in unit_weight else float(re.sub('[^0-9.]', '', unit_weight)))
+        else:
+            return weight
+
+    @staticmethod
+    def parse_date(date_str):
+        try:
+            return parse(date_str).strftime('%Y-%m-%d') if isinstance(date_str, str) else np.nan
+        except ParserError:
+            return np.nan
--- data_extraction.py
+++ data_extraction.py
@@ -3,67 +3,92 @@ import tabula
 import requests
 import boto3
 from io import StringIO
-from urllib.parse import urlparse
-from botocore.client import Config
-from botocore import UNSIGNED
 
-
-class DataExtractor:
-            
+class DataExtractor:            
     def __init__(self, header):
+        """Initializes the DataExtractor with the provided request header."""
         self.header = header
 
     @staticmethod
     def read_rds_table(database_connector, table_name):
+        """
+        Reads a table from a relational database service (RDS).
+
+        Args:
+            database_connector: Instance of the DatabaseConnector class.
+            table_name: Name of the table to read.
+
+        Returns:
+            DataFrame with the content of the table.
+        """
         engine = database_connector.init_db_engine()
-        df = pd.read_sql_table(table_name, engine)
-        return df
+        return pd.read_sql_table(table_name, engine)
 
     def retrieve_pdf_data(self, link):
-        link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+        """
+        Retrieves data from a PDF file.
+
+        Args:
+            link: URL of the PDF file.
+
+        Returns:
+            DataFrame with the content of the PDF file.
+        """
         df_list = tabula.read_pdf(link, pages='all')
-        df = pd.concat(df_list, ignore_index=True)
-        return df
+        return pd.concat(df_list, ignore_index=True)
     
     def list_number_of_stores(self, endpoint):
+        """
+        Retrieves the number of stores from a given endpoint.
+
+        Args:
+            endpoint: URL of the API endpoint.
+
+        Returns:
+            Integer with the number of stores.
+        """
         response = requests.get(endpoint, headers=self.header)
-        print(response.json())  # Print the full response
         return response.json()['number_stores']
     
     def retrieve_stores_data(self, endpoint, num_stores):
-        # Initialize an empty list to store the data from each store
-        data = []
+        """
+        Retrieves data about a certain number of stores from a given endpoint.
 
+        Args:
+            endpoint: URL of the API endpoint.
+            num_stores: Number of stores to retrieve data for.
+
+        Returns:
+            DataFrame with the data about the stores. Returns None if no data could be retrieved.
+        """
+        data = []
         for i in range(1, num_stores + 1):
             try:
                 response = requests.get(f"{endpoint}/{i}", headers=self.header)
                 if response.status_code == 200:
                     data.append(response.json())
-                else:
-                    print(f"There was an error retrieving data for store number {i}")
+
             except Exception as e:
                 print(f"There was an error: {e}")
 
-        # If no data was collected, return None
         if not data:
             return None
 
-        # Create DataFrame from the collected data
-        df = pd.DataFrame(data)
-        
-        return df
-
+        return pd.DataFrame(data)
 
     @staticmethod
     def extract_from_s3(s3_address: str) -> pd.DataFrame:
-        bucket_name = s3_address.split('/')[2]
-        key = '/'.join(s3_address.split('/')[3:])
-        
+        """
+        Extracts data from a file stored in AWS S3.
+
+        Args:
+            s3_address: Address of the file in the S3 bucket.
+
+        Returns:
+            DataFrame with the content of the file.
+        """
+        bucket_name, key = s3_address.split('/')[2], '/'.join(s3_address.split('/')[3:])
         s3 = boto3.client('s3')
         csv_obj = s3.get_object(Bucket=bucket_name, Key=key)
         body = csv_obj['Body'].read().decode('utf-8')
-        
-        data = pd.read_csv(StringIO(body))
-
-        return data
-
+        return pd.read_csv(StringIO(body))

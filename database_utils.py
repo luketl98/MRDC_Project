74eb5ece88bba4fae0c7fa7d8dd89b2286d5a36e
luketl98 <luketl98@hotmail.com>
2023-06-07 00:01:57 +0100

    database_utils & main.py also refactored & working

--- database_utils.py
+++ database_utils.py
@@ -1,56 +1,77 @@
-from sqlalchemy import create_engine
-from sqlalchemy import inspect
-from sqlalchemy.sql import text
 import yaml
+from sqlalchemy import create_engine, inspect, text
+
 
 class DatabaseConnector:
+    """
+    Class to handle all database related operations.
+    """
+    def __init__(self, local_creds=None):
+        """
+        Initialize DatabaseConnector with local database credentials.
+        If local_creds is not provided, database credentials will be read from a file.
+        """
+        if local_creds:
+            self.local_creds = local_creds
+        else:
+            self.local_creds = {
+                'user': 'postgres',
+                'password': 'Aphrodite-186',
+                'host': 'localhost',
+                'port': 5432,
+                'database': 'sales_data'
+            }
+
     @staticmethod
     def read_db_creds(file_path='db_creds.yaml'):
+        """
+        Static method to read database credentials from a yaml file.
+        """
         with open(file_path, 'r') as file:
             db_creds = yaml.safe_load(file)
         return db_creds
-    
+
     def init_db_engine(self, use_local=False):
+        """
+        Initialize the database engine using either local or remote credentials.
+        """
         if use_local:
-            db_user = 'postgres'
-            db_password = 'Aphrodite-186'
-            db_host = 'localhost'
-            db_port = 5432
-            db_database = 'sales_data'
+            db_creds = self.local_creds
         else:
-            creds = self.read_db_creds()
-            db_user = creds['RDS_USER']
-            db_password = creds['RDS_PASSWORD']
-            db_host = creds['RDS_HOST']
-            db_port = creds['RDS_PORT']
-            db_database = creds['RDS_DATABASE']
+            db_creds = self.read_db_creds()
 
-        db_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
-        engine = create_engine(db_url)
-        return engine
+        db_url = f"postgresql://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}"
+        return create_engine(db_url)
 
-    def list_db_tables(self, engine):
+
+    @staticmethod
+    def list_db_tables(engine):
+        """
+        Static method to list all public tables in the database.
+        """
         query = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
         with engine.connect() as connection:
             result = connection.execute(text(query))
             table_names = [row[0] for row in result.fetchall()]
         return table_names
-    
+
     def upload_to_db(self, df, table_name):
-
-        # Define the connection string
-        connection_string = f"postgresql://{'postgres'}:{'Aphrodite-186'}@{'localhost'}:{5432}/{'sales_data'}"
-
-        # Connect to the database
+        """
+        Upload a DataFrame to the specified table in the database.
+        """
+        connection_string = f"postgresql://{self.local_creds['user']}:{self.local_creds['password']}@{self.local_creds['host']}:{self.local_creds['port']}/{self.local_creds['database']}"
         engine = create_engine(connection_string)
 
-        # Set the transaction read-only setting to off
         with engine.begin() as conn:
             conn.execute(text("SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE"))
-
-            # Upload the DataFrame to the database
             df.to_sql(table_name, conn, if_exists='replace', index=False)
 
-    def get_table_schema(self, engine, table_name):
+
+    @staticmethod
+    def get_table_schema(engine, table_name):
+        """
+        Static method to get the schema of a specified table in the database.
+        """
         inspector = inspect(engine)
         return inspector.get_columns(table_name)
+
--- main.py
+++ main.py
@@ -2,68 +2,41 @@ from database_utils import DatabaseConnector
 from data_extraction import DataExtractor
 from data_cleaning import DataCleaning
 
+# Instantiate DatabaseConnector and DataExtractor with necessary parameters
 db_connector = DatabaseConnector()
+header = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
+data_extractor = DataExtractor(header)
+data_cleaning = DataCleaning()
 
-# Create the database engine
+# Initialize database engine
 engine = db_connector.init_db_engine()
 
-# Pass the engine to the list_db_tables method
-table_names = db_connector.list_db_tables(engine)
-
-# Replace 'your_user_data_table' with the correct table name containing user data
+# Extract and clean user data, then upload to database
 user_data_table = 'legacy_users'
-
-if user_data_table in table_names:
+if user_data_table in db_connector.list_db_tables(engine):
     raw_user_data_df = DataExtractor.read_rds_table(db_connector, user_data_table)
+    cleaned_user_data_df = DataCleaning.clean_user_data(raw_user_data_df)
+    db_connector.upload_to_db(cleaned_user_data_df, 'dim_users')
 else:
     print(f"User data table '{user_data_table}' not found")
 
-# Clean the user data
-cleaned_user_data_df = DataCleaning.clean_user_data(raw_user_data_df)
-
-# Upload the cleaned user data to the sales_data database
-db_connector.upload_to_db(cleaned_user_data_df, 'dim_users')
-
-# Instantiate the classes
-header = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
-data_extractor = DataExtractor(header)
-
-# TASK 4
+# Extract, clean and upload card data from PDF
 link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
 raw_card_data_df = data_extractor.retrieve_pdf_data(link)
-
-data_cleaning = DataCleaning()
 clean_card_data_df = data_cleaning.clean_card_data(raw_card_data_df)
-
 db_connector.upload_to_db(clean_card_data_df, 'dim_card_details')
 
+# Extract, clean and upload store data from API
 number_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
 store_data_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details"
-
-cleaner = DataCleaning()
-
-# Step 2: Get the number of stores
 num_stores = data_extractor.list_number_of_stores(number_stores_endpoint)
-
-# Retrieve and clean store data
 store_data = data_extractor.retrieve_stores_data(store_data_endpoint, num_stores)
-clean_store_data = cleaner.clean_store_data(store_data)
-
-# Export to CSV
-clean_store_data.to_csv('clean_store_data.csv', index=False)
-
-# Step 5: Upload cleaned data to database
-# Replace with your function to upload data to database
+clean_store_data = data_cleaning.clean_store_data(store_data)
 db_connector.upload_to_db(clean_store_data, 'dim_store_details')
 
-# Extract product data from S3
+# Extract, clean and upload product data from S3
 s3_address = "s3://data-handling-public/products.csv"
 raw_product_data_df = DataExtractor.extract_from_s3(s3_address)
-raw_product_data_df.to_csv('raw_product_data.csv')
-
-
-# Clean the product data
 clean_product_data_df = DataCleaning.clean_products_data(raw_product_data_df)
-
-# Upload the cleaned product data to the sales_data database
 db_connector.upload_to_db(clean_product_data_df, 'dim_products')
+
